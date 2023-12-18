9298093a0bfa5d819f7ecec4e1fb78be95f55a85
dkiriinya <dkiriinya06sc@gmail.com>
2023-12-18 21:43:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution here
+function findMatching(arr, driver) {
+    let lowercaseddriver = driver.toLowerCase()
+    return arr.filter(function (name) {
+        let lowercasedname = name.toLowerCase()
+        return lowercasedname.includes(lowercaseddriver);
+    });
+}
+function fuzzyMatch(arr, letters) {
+    const lowercasedLetters = letters.toLowerCase();
+
+    return arr.filter(function (driver) {
+        const lowercasedDriver = driver.toLowerCase();
+        return lowercasedDriver.startsWith(lowercasedLetters);
+    });
+}
+function matchName(arr, name) {
+    const lowercasedname = name.toLowerCase()
+    return arr.filter(function(driver){
+        return driver.name === name
+    })
+}
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+
+console.log(matchName(drivers,"Bobby"))
